c37b00ec5a5ce523acda21e1715cab5e3b39bd0c
Faheema Tamton <fymatampc@gmail.com>
2026-06-08 19:13:28 +0530

    Update pipeline.py

--- rag/pipeline.py
+++ rag/pipeline.py
@@ -1,9 +1,4 @@
-from ingestion.text_ingest import extract_text
-from ingestion.table_ingest import extract_tables
-from ingestion.image_ingest import extract_images_text
-from rag.text_splitter import split_text
-from rag.vector_store import create_store
-from agent.agent import create_agent
+
 
 agent = create_agent()
 
