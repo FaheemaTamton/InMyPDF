42b78cf98f2d2d95f16891c1bdba975011ddfdd9
FaheemaTamton <fymatampc@gmail.com>
2026-06-20 09:25:20 +0530

    Update InMypdf

--- ingestion/image_ingest.py
+++ ingestion/image_ingest.py
@@ -1,13 +1,4 @@
-import pytesseract
-from PIL import Image
-import pdfplumber
-import io
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
-
-def extract_images_text(pdf_path) -> str:
-    image_text = ""
 
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
