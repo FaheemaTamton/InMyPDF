55def0e823d5ac9f112f9b9a4312329904ea4eab
FaheemaTamton <fymatampc@gmail.com>
2026-06-30 16:47:49 +0530

    Update InMypdf

--- ingestion/image_ingest.py
+++ ingestion/image_ingest.py
@@ -6,5 +6,21 @@ import io
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
+def extract_images_text(pdf_path) -> str:
+    image_text = ""
+
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            for image in page.images:
+                cropped = page.crop((
+                    image["x0"],
+                    image["top"],
+                    image["x1"],
+                    image["bottom"]
+                ))
+
+                img = cropped.to_image(resolution=300).original
+                text = pytesseract.image_to_string(img)
+                image_text += text + "\n"
 
     return image_text
